449148b3619e8db019d7dff44c9d3246584fa419
John <99333852+Quinnus@users.noreply.github.com>
2026-02-26 18:09:27 +0000

    modified App.css form entry to make it more visually appealing

--- my-app/src/pages/PostJob.jsx
+++ my-app/src/pages/PostJob.jsx
@@ -24,28 +24,30 @@ export default function PostJob() {
     return (
         <div className="page">
             <h1>Post New Job</h1>
-            <p>Please enter the details of the job you would like to post</p>
-            <p>You will need to provide a description of the job, and your offered price</p>
-            <p>Once posted, suppliers can follow up with you directly</p>
-            <h4>Job Name</h4>
-            <textarea style={{margin: '10px 0px'}} id={"inputjobtitle"} rows="2" cols="60" placeholder={"e.g." +
-                "Leaking Tap URGENT"} onChange={(event) => setJobTitle(event.target.value)}/>
-            <h4>Job Description</h4>
-            <textarea style={{margin: '10px 0px'}} id={"inputjobdesc"} rows="3" cols="60" placeholder={"e.g. The hot" +
-                " water tap in my bathroom is constantly dripping.  My husband tried to fix it but he's made it" +
-                " worse."} onChange={(event) => setJobDescription(event.target.value)}/>
-            <h4>Fee (€)</h4>
-            <textarea style={{margin: '10px 0px'}} id={"inputjobprice"} rows="2" cols="60" placeholder={"format" +
-                " 123.45"} onChange={(event) => setJobPrice(event.target.value)}/>
-            <br/>
-            <button style={{margin: '10px 80px'}} onClick={handlePreviewJob}>Preview</button>
+            <form>
+                <p>Please enter the details of the job you would like to post</p>
+                <p>You will need to provide a description of the job, and your offered price</p>
+                <p>Once posted, suppliers can follow up with you directly</p>
+                <h4>Job Name</h4>
+                <input type="text" id={"inputjobtitle"} placeholder={"e.g." +
+                    "Leaking Tap URGENT"} onChange={(event) => setJobTitle(event.target.value)}/>
+                <h4>Job Description</h4>
+                <textarea id={"inputjobdesc"}
+                          placeholder={"e.g. The hot" +
+                              " water tap in my bathroom is constantly dripping"}
+                          onChange={(event) => setJobDescription(event.target.value)}></textarea>
+                <h4>Fee (€)</h4>
+                <input type="number" min="1" id={"inputjobprice"} placeholder={"format" +
+                    " 123"} onChange={(event) => setJobPrice(event.target.value)}/>
+                <br/>
+                <button type="button" onClick={handlePreviewJob}>Preview</button>
 
-            <button style={{margin: '10px 40px'}} onClick={handleCancelPost}>Cancel</button>
+                <button type="button" onClick={handleCancelPost}>Cancel</button>
 
-            {isDisplayed && <PreviewJob onClickEdit={closePreview}
-                                        jobPreviewTitle={jobTitle}
-                                        jobPreviewDescription={jobDescription}
-                                        jobPreviewPrice={jobPrice}/>}
+                {isDisplayed && <PreviewJob onClickEdit={closePreview}
+                                            jobPreviewTitle={jobTitle}
+                                            jobPreviewDescription={jobDescription}
+                                            jobPreviewPrice={jobPrice}/>}</form>
         </div>
     );
 }
