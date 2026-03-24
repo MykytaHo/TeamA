101ecb12486b87dc9c5c9309da841a53caacc2a6
Rujing <delia.rj.xu@outlook.com>
2026-03-24 11:15:31 +0000

    Revert "feat: implement core Client and Supplier workflows"
    This reverts commit b0e2ef47b0757b665c05f8ad62170fbdef220d2f.

--- my-app/src/firebase.js
+++ my-app/src/firebase.js
@@ -3,7 +3,6 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -23,4 +22,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app); 
 export const db = getFirestore(app); 
-export const storage = getStorage(app); 
+export const storage = getStorage(app);
